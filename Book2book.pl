621db2c96a8e32f909dc93c8b9c704755732a1f6
Alex Cher <bsddec@gmail.com>
2011-04-19 11:52:35 +0300

    script to rename files to lower case

---
+++ Book2book.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use Cwd;
+
+print "**   rename Book.xml/Question.xml names to lower case   **\n";
+
+if(defined($ARGV[0])){
+	$curdir = $ARGV[0];
+}
+else{
+	$curdir = getcwd;
+}
+print "rename books in $curdir directory\n";
+opendir(CURDIR, $curdir) or die "Cannot read directory $curdir\n";
+@books = grep {/Book/ig} readdir CURDIR;
+#print @books;
+foreach(@books){
+	&bookRename("$curdir$_");
+};
+close(CURDIR);
+
+sub bookRename{
+	my $bookDir = $_[0];
+	print "rename in directory: $_[0]\n";
+	opendir(BOOKDIR, $bookDir) or die "Cannot read book directory $bookDir\n";
+	@bookFiles = grep {/Book.xml|Questions.xml/ig} readdir BOOKDIR;
+	foreach(@bookFiles){
+		my $oldFile = "$bookDir/$_";
+		my $newFile = "$bookDir/".lc;
+		print "old file: $oldFile\n";
+		print "new file: $newFile\n";
+		rename($oldFile,$newFile);
+	}
+	close(BOOKDIR);
+}
+
+
